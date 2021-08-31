89687d11babaeb2b78f307d2344f427fd3538b14
mddanishyusuf <mddanishyusuf@gmail.com>
2021-08-31 23:58:03 +0530

    fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import {FiTwitter, FiGithub} from 'react-icons/fi'
+import { FiTwitter, FiGithub } from "react-icons/fi"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,13 +12,20 @@ const IndexPage = () => (
     <Projects />
 
     <div className="contact">
-            <h3 className="h3-heading">Contact</h3>
-            <p>If you have <span>any question</span> or wants to convert your <span>idea</span> into MVP in <span>days</span> just drop mail at <span>hello@mohddanish.me</span></p>
-            <ul className="social-links">
-      <li><a href="https://twitter.com/mvpstackhq" target="_blank"><FiGithub /> <span>Twitter</span></a></li>
-      <li><a href="https://github.com/mvpstack" target="_blank"><FiTwitter /> <span>GitHub</span></a></li>
-              </ul>
-        </div>
+      <h3 className="h3-heading">Contact</h3>
+      <p>
+        If you have <span>any question</span> or wants to convert your{" "}
+        <span>idea</span> into MVP in <span>days</span> just drop mail at{" "}
+        <span>hello@mohddanish.me</span>
+      </p>
+      <ul className="social-links">
+        <li>
+          <a href="https://twitter.com/mddanishyusuf" target="_blank">
+            <FiGithub /> <span>Twitter</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 )
 
